1ad71504a0625c180fdcdb93cff4726e12142914
Tomas Simko <72190589+TomassSimko@users.noreply.github.com>
2023-04-29 17:16:39 +0200

    initial commit

---
+++ wuav-app/wuav_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  wuav_appApp.swift
+//  wuav-app
+//
+//  Created by Legind on 29/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct wuav_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
